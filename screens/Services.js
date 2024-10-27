a758c9f509e37f5d1200ac982b119797a6818cb8
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-10-27 21:13:23 +0000

    udpate changes

--- screens/Services.js
+++ screens/Services.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { View, FlatList, StyleSheet, Alert, TextInput } from 'react-native';
+import { View, FlatList, StyleSheet, Alert, TextInput , TouchableOpacity } from 'react-native';
 import supabase from '../supabaseClient'; // Ensure the correct import path
 import { useNavigation } from '@react-navigation/native';
 import ServiceItem from './services/ServiceItem';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import Loading from '../component/loadingComponent/loading';
 import Error from '../component/loadingComponent/Error';
 import LocationDisplay from '../component/LocationDisplay'; // Import the LocationDisplay component
@@ -73,12 +74,17 @@ const Services = () => {
     <View style={styles.container}>
       <LocationDisplay /> {/* Include the LocationDisplay component */}
 
-      <TextInput
-        style={styles.searchBar}
-        placeholder="Search services..."
-        value={searchQuery}
-        onChangeText={handleSearch}
-      />
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchBar}
+          placeholder="Search products..."
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+        />
+        <TouchableOpacity style={styles.filterButton} onPress={() => console.log('Filter pressed')}>
+          <Icon name="filter" size={20} color="#007bff" />
+        </TouchableOpacity>
+      </View>
       <FlatList
         data={filteredServices}
         renderItem={renderItem}
@@ -94,12 +100,30 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: '#f5f5f5',
   },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
   searchBar: {
+    flex: 1,
     height: 40,
     borderColor: '#ccc',
     borderWidth: 1,
-    borderRadius: 5,
+    borderRadius: 8,
     paddingHorizontal: 10,
+    marginRight: 8,
+  },
+  filterButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 8,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 8,
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     marginBottom: 16,
   },
 });
